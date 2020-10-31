71500ba3934c7019bfda4aa94621836b2356bf8a
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2020-11-01 02:24:12 +0300

    update delete actions added.

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
-    <div class="todos">
-      <div v-for="todo in todos" :key="todo.id" class="todo">
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span> <span class="incomplete-box"></span> = Incomplete </span>
+      <span> <span class="complete-box"></span> = Complete </span>
+    </div>
+    <div>
+      <button class="hide" @click="toggleVisibility">Hide Todos</button>
+    </div>
+    <div v-if="isVisible" class="todos">
+      <div
+        v-for="todo in todos"
+        :key="todo.id"
+        class="todo"
+        @dblclick="updateTodo(todo)"
+        v-bind:class="{ 'is-complete': todo.completed }"
+      >
         <p>{{ todo.title }}</p>
+        <i v-on:click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
       </div>
     </div>
   </div>
@@ -10,20 +25,40 @@
 <script>
 import { useStore } from 'vuex'
 import { computed, onMounted } from 'vue'
+import { useToggle } from '@/composables/useToggle'
 export default {
   name: 'Todos',
   setup () {
     const store = useStore()
+
     const todos = computed(() => store.state.todos)
     onMounted(() => {
       store.dispatch('onFetchTodos')
     })
+    const deleteTodo = id => {
+      store.dispatch('onDeleteTodo', id)
+    }
+    const updateTodo = todo => {
+      const updatedTodo = {
+        title: todo.title,
+        id: todo.id,
+        completed: !todo.completed
+      }
+      store.dispatch('onUpdateTodo', updatedTodo)
+    }
+    // visibility
+    const { isVisible, toggleVisibility } = useToggle()
     return {
-      todos
+      todos,
+      isVisible,
+      toggleVisibility,
+      deleteTodo,
+      updateTodo
     }
   }
 }
 </script>
+
 <style scoped>
 .todos {
   display: grid;
