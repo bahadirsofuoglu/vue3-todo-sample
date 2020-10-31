af2938ab82d89686fb1f09478a1c622fb67b3190
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2020-11-01 01:55:21 +0300

    vuex store created

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,24 @@
-import { createStore } from "vuex";
-
+import { createStore } from 'vuex'
+import Axios from 'axios'
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  state: {
+    todos: []
+  },
+  mutations: {
+    [ACTION_TYPES.fetchTodos]: (state, todos) => (state.todos = todos)
+  },
+  actions: {
+    onFetchTodos: async ({ commit }) => {
+      const response = await Axios.get(
+        'https://jsonplaceholder.typicode.com/todos'
+      )
+      commit(ACTION_TYPES.fetchTodos, response.data)
+    }
+  }
+})
+export const ACTION_TYPES = {
+  fetchTodos: 'fetchTodos',
+  addTodo: 'addTodo',
+  deleteTodo: 'deleteTodo',
+  updateTodo: 'updateTodo'
+}
