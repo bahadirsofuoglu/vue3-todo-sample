e2e3cac649c0fa8b486626f2205979d804c282ed
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2020-11-01 02:20:04 +0300

    filter-action added store

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
     <AddTodo />
+    <FilterTodos />
     <Todos />
   </div>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
-
 import AddTodo from '@/components/AddTodo.vue'
 import Todos from '@/components/Todos.vue'
-
+import FilterTodos from '@/components/FilterTodos.vue'
 export default {
   name: 'App',
   components: {
+    AddTodo,
     Todos,
-    AddTodo
+    FilterTodos
   }
 }
 </script>
--- src/components/FilterTodos.vue
+++ src/components/FilterTodos.vue
@@ -2,11 +2,12 @@
   <div>
     <form @submit="filterTodos">
       <input type="number" v-model="limit" placeholder="Add to do ..." />
-      <input @click.prevent="filterTodos" type="submit" value="Filter" />
+      <input v-on:click.prevent="filterTodos" type="submit" value="Filter" />
     </form>
     <br />
   </div>
 </template>
+
 <script>
 import { ref } from 'vue'
 import { useStore } from 'vuex'
@@ -25,3 +26,21 @@ export default {
   }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
